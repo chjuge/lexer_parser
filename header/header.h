329f57daf435a57ff5acdee29e81716c51b5b757
Marylee Provement <mproveme@at-r1.msk.21-school.ru>
2022-10-03 17:18:21 +0300

    added check for special characters

--- header/header.h
+++ header/header.h
@@ -5,15 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mproveme <mproveme@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/03 16:33:41 by mproveme          #+#    #+#             */
-/*   Updated: 2022/10/03 16:34:55 by mproveme         ###   ########.fr       */
+/*   Created: 2022/10/03 16:58:31 by mproveme          #+#    #+#             */
+/*   Updated: 2022/10/03 17:15:10 by mproveme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
 
-#include <unistd.h>
-#include <stdio.h>
-
+# define DEL ' '
+# define DELINT 4
+# define COM '\''
+# define COMINT 1
+# define COM2 '\"'
+# define COM2INT 2
+# define PIPE '|'
+# define PIPEINT 3
+# define REDG '>'
+# define REDL '<'
+# define REDINT 5
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "./header/header.h"
 int	string_run(char *str);
+int	check_sp_ch(char c);
 
 int	main(int argc, char** argv, char **envp)
 {
@@ -24,3 +25,26 @@ int	string_run(char *str)
 	printf("\n");
 	return (i);
 }
+
+int	check_sp_ch(char c)
+{
+	if (c == COM)
+		return (COMINT);
+	else if (c == COM2)
+		return (COM2INT);
+	else if (c == PIPE)
+		return (PIPEINT);
+	else if (c == DEL)
+		return (DELINT);
+	else if (c == REDG || c == REDL)
+		return (REDINT);
+	return (0);
+}
+
+
+// avaliable types:
+// '  -  comma				com
+// "  -  double comma		com2
+// |  -  pipe				pipe
+// < > << >> - redirection	redir
+// all other - words 		word
